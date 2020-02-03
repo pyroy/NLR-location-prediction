dede4bd3c6f0b7e37789c8a3fbad654535d913fa
pyroy <35765207+pyroy@users.noreply.github.com>
2020-02-03 12:07:57 +0100

    Cleaned up comments

--- src/location_selection.py
+++ src/location_selection.py
@@ -1,36 +1,6 @@
 from nodes import *
 import numpy as np
 
-#OSMInterface = OSMInterfaceOBJ()
-#OSMInterface.load_location("Bremm", origin = (7.0975, 50.1089) )
-
-#print( 5955007897 in OSMInterface.WAY_INFO[ OSMInterface.current_location ].keys() )
-
-#sizes of area in squared km
-A = {}
-B = {}
-C = {}
-# F["area"] = ("between", [10,15])
-# F["area"] = ("greaterthan", 0.00000707)  # kleine auto
-# F["area"] = ("greaterthan", 0.00001053)  # middel auto
-# F["area"] = ("greaterthan", 0.00001203)  # grote auto
-# F["area"] = ("greaterthan", 0.00001243)  # groot busje
-# F["area"] = ("greaterthan", 0.00002125)  # kleine vrachtwagen
-# F["area"] = ("greaterthan", 0.00004781)  # normale vrachtwagen
-A["area"] = ("greaterthan", 0.00000135)  # fiets
-# F["area"] = ("greaterthan", 0.0000005)   # personen drone
-# A["area"] = ("greaterthan", 0.00005115)  # militaire drone
-A["bruhhhhhhhhhhhhhhhhhhhhh"] = ["highway", "railway", "waterway", "natural"]
-
-# F["area"] = ("greaterthan", 0.00003047)  # bus
-# F["area"] = ("greaterthan", 0.00003022)  # buk
-B["area"] = ("greaterthan", 0.00001147)  # hhmmmmv
-# F["area"] = ("greaterthan", 0.00001537)  # hmmmv met mmr
-# F["area"] = ("greaterthan", 0.00002132)  # kleine tank
-# F["area"] = ("greaterthan", 0.00003587)  # grote tank
-# F["area"] = ("greaterthan", 0.00002100)  # raket systeem
-# F["area"] = ("greaterthan", 0)  # alle polygons
-
 def query(OSMInterface, features={}):
 
     all_ways = OSMInterface.get_streets()
@@ -72,12 +42,6 @@ def area(o):
 
     return abs(a)/0.34*1800/1000000
 
-#m = 654706368
-#p = 527798548
-#print(OSMInterface.get_node_from_id(p).lat, OSMInterface.get_node_from_id(p).lon)
-#print(o)
-#print( area( o ) )
-
 def select(way_id, way, features, OSMInterface):
     a = area( [OSMInterface.get_node_from_id(n).get_pos() for n in way] )
     if "area" in features.keys():
@@ -89,14 +53,11 @@ def select(way_id, way, features, OSMInterface):
         elif relation == "greaterthan":
             if a < features["area"][1]: return False
 
-    if "bruhhhhhhhhhhhhhhhhhhhhh" in features.keys():
+    if "wanted_features" in features.keys():
         f = False
-        for ffff in features[ "bruhhhhhhhhhhhhhhhhhhhhh" ]:
-            if ffff in OSMInterface.WAY_INFO[ OSMInterface.current_location ][way_id].keys():
+        for ffff in features[ "wanted_features" ]:
+            if ffff in OSMInterface.WAY_INFO[way_id].keys():
                 f = True
         if not f: return False
 
     return True
-
-#a = [i[0] for i in query(OSMInterface, A)]
-#print( len( a ) )
